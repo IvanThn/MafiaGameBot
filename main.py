b69cb20ccdf7df5012f56f4f1c85e9ce895a8e3a
Ivan Terekhin <iven.thn@gmail.com>
2024-02-25 18:07:46 +0300

    Добавление функций SELECT_USER_ID и UPDATE_IN_GAME_ID. Перенос основного кода на sqlalchemy

--- main.py
+++ main.py
@@ -1,37 +1,20 @@
 from telebot import TeleBot
 from telebot import types
-import sqlite3
 
 import settings
+
+from repositories import *
 
 
 bot = TeleBot(settings.TOKEN)
 
-# INSERT_USER_DATA = '''
-#     INSERT INTO users VALUES(?, ?, ?, ?)
-# '''
-#
-# SELECT_USER_IN_GAME = '''
-#     SELECT user_name, cur_game_id FROM users WHERE cur_game_id IS NOT NULL
-# '''
 
-SELECT_USER_ID = '''
-    SELECT user_id FROM users
-'''
-
-UPDATE_IN_GAME_ID = '''
-    UPDATE users SET cur_game_id = ? WHERE user_id = ?
-'''
-
-
+@bot.message_handler(commands=['start'])
 def start(message):
     if message.chat.type == 'private':
         bot.send_message(message.chat.id, "Hi! I'm MafiaGame bot!")
     else:
-        connect = sqlite3.connect('data.db')
-        cursor = connect.cursor()
-
-        users_in_game = cursor.execute(SELECT_USER_IN_GAME).fetchall()
+        users_in_game = select_users_in_game()
         users_in_game_list = [name[0] for name in users_in_game]
         markup = types.InlineKeyboardMarkup()
         login_btn = types.InlineKeyboardButton('Присоединиться', callback_data='login')
@@ -42,25 +25,18 @@ def start(message):
 @bot.callback_query_handler(func=lambda callback: True)
 def callback_message(callback):
     if callback.data == 'login':
-
-        connect = sqlite3.connect('data.db')
-        cursor = connect.cursor()
-
-        users_id = cursor.execute(SELECT_USER_ID).fetchall()
-        id_list = [id[0] for id in users_id]
+        id_list = select_usr_id()
         if callback.from_user.id not in id_list:
-            cursor.execute(INSERT_USER_DATA, (callback.from_user.id, callback.from_user.username, 0, None))
-            connect.commit()
+            insert_user(callback.from_user.id, callback.from_user.username)
 
-        users_in_game = cursor.execute(SELECT_USER_IN_GAME).fetchall()
+        users_in_game = select_users_in_game()
         in_game_id_list = [id[1] for id in users_in_game]
         if callback.message.chat.id not in in_game_id_list:
-            cursor.execute(UPDATE_IN_GAME_ID, (callback.message.chat.id, callback.from_user.id))
+            update_in_game_id(callback.message.chat.id, callback.from_user.id)
         else:
-            cursor.execute(UPDATE_IN_GAME_ID, (None, callback.from_user.id))
-        connect.commit()
+            update_in_game_id(None, callback.from_user.id)
 
-        users_in_game = cursor.execute(SELECT_USER_IN_GAME).fetchall()
+        users_in_game = select_users_in_game()
         users_in_game_list = [name[0] for name in users_in_game]
         markup = types.InlineKeyboardMarkup()
         login_btn = types.InlineKeyboardButton('Присоединиться', callback_data='login')
--- repositories.py
+++ repositories.py
@@ -1,7 +1,8 @@
-from sqlalchemy import select
+from sqlalchemy import select, update
 
 from sqlalchemy.orm import Session
 from ORM import engine, Users
+
 
 # session = Session(engine)
 #
@@ -26,8 +27,24 @@ def select_users_in_game():
     session = Session(engine)
 
     users = select(Users).where(Users.cur_game_id.isnot(None))
+    res = []
     for user in session.scalars(users):
-        return user.user_name, user.cur_game_id
+        res.append((user.user_name, user.cur_game_id))
+    return res
 
 
+def select_usr_id():
+    session = Session(engine)
 
+    users = select(Users.user_id)
+    res = []
+    for user_id in session.scalars(users):
+        res.append(user_id)
+    return res
+
+
+def update_in_game_id(game_id: int or None, user_id: int):
+    session = Session(engine)
+
+    session.query(Users).filter(Users.user_id == user_id).update({'cur_game_id': game_id})
+    session.commit()
